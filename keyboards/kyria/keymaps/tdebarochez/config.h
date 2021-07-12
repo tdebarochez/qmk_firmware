505ca0e1c37ebfcc2f686f8f9c9aa150180420b0
Thomas de Barochez <thomas.barochez+github@gmail.com>
2021-07-12 09:08:17 +0200

    tdebarochez, remove pong

--- keyboards/kyria/keymaps/tdebarochez/config.h
+++ keyboards/kyria/keymaps/tdebarochez/config.h
@@ -25,7 +25,7 @@
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 150
+  #define RGBLIGHT_LIMIT_VAL 250
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
