e33201bd8396ee6daae9973f26dd9a6a74dd1c84
Thomas de Barochez <thomas.barochez+github@gmail.com>
2021-07-12 21:17:11 +0200

    fix encoders

--- keyboards/kyria/keymaps/tdebarochez/keymap.c
+++ keyboards/kyria/keymaps/tdebarochez/keymap.c
@@ -202,17 +202,17 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     }
     else if (index == 1) {
-        // Page up/Page down
+        //  Brightness up/down
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_SLCK);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_PAUS);
         }
     }
     return true;
